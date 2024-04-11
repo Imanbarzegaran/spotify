fac97cb1e1218c5ec24c6a0a39530986f6bcf27d
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-04-11 21:15:05 +0330

    add Sighn in button
    add Sighn In button in welcom  ViewController

--- spotify/spotify/Controllers/Other/WelcomViewController.swift
+++ spotify/spotify/Controllers/Other/WelcomViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class WelcomViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In With Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      title = "Spotify"
+        title = "Spotify"
         view.backgroundColor = .systemGreen
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
     }
    
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInButton.frame = CGRect(
+            x: 20,
+            y: view.height-50-view.safeAreaInsets.bottom,
+            width: view.width-40,
+            height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        let vc = AuthViewController()
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
 }
--- spotify/spotify/Managers/AuthManager.swift
+++ spotify/spotify/Managers/AuthManager.swift
@@ -11,6 +11,9 @@ import Foundation
 final class AuthManager {
     static let Shared = AuthManager()
     
+    static let clientID = "36dca6a57a574d8fbf787c2078af5265"
+    static let clientSecret = "83b9837cfc5b4f8f94b20f46169acc0d"
+    
     private init() {}
     
     var isSignedIn: Bool {
